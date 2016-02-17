6df8be4b6df1e9fbdd9bafb535961a36efded9aa
Austin Byers <austinbyers@uchicago.edu>
2016-02-17 14:06:40 -0600

    cd/ls directory traversal

--- globusfs.py
+++ globusfs.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-
 # Copyright (c) 2016 Austin Byers <austin.b.byers@gmail.com>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
-
-"""Mount a Globus endpoint over FUSE."""
-
+"""Mount a Globus endpoint with FUSE."""
 
 import argparse
 import os
@@ -22,24 +19,55 @@ class GlobusFS(Operations):
 
     def __init__(self, endpoint):
         # Get credentials and activate endpoint.
-        auth_result = goauth.get_access_token()  # Don't keep access token around in memory longer than needed.
+        auth_result = goauth.get_access_token()
         self.api = TransferAPIClient(username=auth_result.username, goauth=auth_result.token)
         self.endpoint = endpoint
-        self.api.endpoint_autoactivate(endpoint)
+        _, _, data = self.api.endpoint_autoactivate(endpoint)
+        print data['message']
+
+        # Cache file metadata in memory.
+        now = time.time()
+        root_stat = {'st_atime': now, 'st_mtime': now, 'st_ctime': now,
+                     'st_nlink': 2, 'st_mode': (stat.S_IFDIR | 0755)}
+        self.files = {'/': root_stat}  # Map filepath to stat() file info dictionaries.
+        self.dirs = {}  # Map dirname to list of filenames.
+
+
+    def access(self, path, amode):
+        """Pre-emptively read file metadata when accessing a new directory."""
+        print 'access', path, amode
+        if path not in self.dirs:
+            _, _, data = self.api.endpoint_ls(self.endpoint, path=path)
+
+            # Add list of file names to directory (for readdir())
+            self.dirs[path] = [x['name'] for x in data['DATA']]
+
+            # Add file metadata (for getattr())
+            for file_info in data['DATA']:
+                now = time.time()
+                f_type = stat.S_IFDIR if file_info['type'] == 'dir' else stat.S_IFREG
+                permissions = int(file_info['permissions'])
+
+                self.files[os.path.join(path, file_info['name'])] = {
+                    'st_atime': now,
+                    'st_mtime': now,  # TODO: last_modified is an actual field we can use
+                    'st_ctime': now,
+                    'st_nlink': 2,
+                    'st_mode': (f_type | permissions)
+                }
+        return 0
 
     def getattr(self, path, fh=None):
         print 'getattr', path
-        now = time.time()
-        return {'st_atime': now, 'st_mtime': now, 'st_ctime': now,
-                'st_nlink': 2, 'st_mode': (stat.S_IFDIR | 0755)}
+        return self.files[path]
     
     def mkdir(self, path, mode):
-        print 'mkdir: ', path
+        print 'mkdir', path
         self.api.endpoint_mkdir(self.endpoint, path)
 
     def readdir(self, path, fh):
-        code, msg, data = self.api.endpoint_ls(self.endpoint)
-        return [x['name'] for x in data['DATA']]
+        print 'readdir', path
+        return ['.', '..'] + self.dirs[path]
 
 
 def main():
